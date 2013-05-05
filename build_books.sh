ccb5cb8f98eee1e68560a12eb0d6ecfb0da1580a
Lee Newson <lnewson@redhat.com>
2013-05-05 13:51:35 +1000

    Added initial implementaiton of Translation Build scripts

---
+++ build_books.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+
+function cleanHTMLDir()
+{
+	CLEAN_DIR=$1
+
+	if [ -d /var/www/html/${CLEAN_DIR} ] || [ -e /var/www/html/${CLEAN_DIR} ]
+	then
+		rm -rf ${CLEAN_DIR}
+	fi
+
+}
+
+function backupAndCleanHTMLDir()
+{
+	CLEAN_DIR=$1
+
+	if [ -d /var/www/html/${CLEAN_DIR} ] || [ -e /var/www/html/${CLEAN_DIR} ]
+	then
+		if [ -d /var/www/html/backup/${CLEAN_DIR} ] || [ -e /var/www/html/backup/${CLEAN_DIR} ]
+		then
+			rm -rf /var/www/html/backup/${CLEAN_DIR}
+		fi
+
+		cp -r /var/www/html/${CLEAN_DIR} /var/www/html/backup/
+		rm -rf /var/www/html/${CLEAN_DIR}
+	fi
+
+}
+
+TMP_DIR=/tmp/buildbooks
+BOOKNAME=Book
+EXPECTED_ARGS=2
+
+if [ "$#" -lt ${EXPECTED_ARGS} ]
+then
+	echo ERROR! Expected more arguments.
+	exit 1
+fi
+
+# Get the suffix on the directory
+DIR_SUFFIX=$1
+shift
+
+while (( "$#" ))
+do
+	IFS='=' read -ra ADDR <<< "$1"
+
+  # Extract the language, Common Content language and CSP id
+  # from the command line argument
+	BUILD_LANG=${ADDR[0]}
+	COMMON_LANG=${ADDR[1]}
+	CSPID=${ADDR[2]}
+
+  # Shift the arguments down
+  shift
+
+	# Start with a clean temp dir for every build
+	if [ -d ${TMP_DIR}${DIR_SUFFIX} ]
+	then
+		rm -rf ${TMP_DIR}${DIR_SUFFIX}
+	fi
+	
+	mkdir ${TMP_DIR}${DIR_SUFFIX}
+
+	# Enter the temp directory
+	pushd ${TMP_DIR}${DIR_SUFFIX}
+
+    date > build.log
+
+		echo csprocessor build --lang ${BUILD_LANG} --server --editor-links --show-report --permissive --common-content ${COMMON_LANG} --output ${BOOKNAME}.zip ${CSPID}
+		csprocessor build --lang ${BUILD_LANG} --server --editor-links --show-report --permissive --common-content ${COMMON_LANG} --output ${BOOKNAME}.zip ${CSPID}
+
+		# If the csp build failed then continue to the next item
+		if [ $? != 0 ]
+		then
+			if [ -d /var/www/html/${CSPID} ]
+      then
+        rm -rf /var/www/html/${CSPID}
+      fi
+
+			mkdir /var/www/html/${BUILD_LANG}/${CSPID}
+			cp build.log /var/www/html/${BUILD_LANG}/${CSPID}
+
+			continue
+		fi		
+
+
+		unzip ${BOOKNAME}.zip
+
+		# The zip file will be extracted to a directory name that 
+		# refelcts the name of the book. We don't know this name,
+		# but we can loop over the subdirectories and then break
+		# once we have processed the first directory.
+		for dir in ./*/
+		do
+
+			# Enter the extracted book directory
+			pushd ${dir}
+
+				echo "publican build --formats=html-single --langs=${BUILD_LANG} &> publican.log"
+
+				publican build --formats=html-single --langs=${BUILD_LANG} &> publican.log
+
+				# If the publican build fails then put the log in the html dir
+				if [ $? != 0 ]
+				then
+					mkdir /var/www/html/${BUILD_LANG}/${CSPID}
+					cp publican.log /var/www/html/${BUILD_LANG}/${CSPID}
+
+					continue
+				fi
+
+				backupAndCleanHTMLDir ${BUILD_LANG}/${CSPID}
+				mkdir -p /var/www/html/${BUILD_LANG}/${CSPID}
+				
+				cp -R tmp/${BUILD_LANG}/html-single/* /var/www/html/${BUILD_LANG}/${CSPID}
+
+				cleanHTMLDir ${BUILD_LANG}/${CSPID}
+        mkdir -p /var/www/html/${BUILD_LANG}/${CSPID}
+
+				cp -R tmp/${BUILD_LANG}/html-single/* /var/www/html/${BUILD_LANG}/${CSPID}
+
+
+			popd
+			
+			# we only want to process one directory
+			break
+
+		done
+
+	popd
+done
